a4e3bec83c1cd10cc5554bd5a792640585dd9233
g22307 <g22307@kagawa.kosen-ac.jp>
2026-07-01 13:13:08 +0900

    UIを実装

---
+++ Assets/Projects/Scripts/UI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Projects.Scripts
+{
+    public class UI : MonoBehaviour
+    {
+        [SerializeField] private UIDocument document;
+
+        private void Start()
+        {
+            var button = document.rootVisualElement.Q<Button>("QuitButton");
+            button.clicked += () =>
+            {
+                Debug.Log("Quit");
+#if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+            };
+        }
+    }
+}
